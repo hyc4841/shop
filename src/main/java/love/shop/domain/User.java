504b657eab9bb916cd0b53777ae3cc73ad1ab93b
hyc4841 <dbscjf4841@naver.com>
2024-12-04 02:09:44 +0900

    jwt 기반 로그인

--- src/main/java/love/shop/domain/User.java
+++ src/main/java/love/shop/domain/User.java
@@ -25,4 +25,3 @@ public class User {
         this.memo = memo;
     }
 }
-
---
+++ src/main/java/love/shop/web/login/jwt/JwtToken.java
@@ -0,0 +1,14 @@
+package love.shop.web.login.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class JwtToken {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+}
