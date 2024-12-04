60e5ece037b31c38c294fd137cfb24ac316888b3
hyc4841 <dbscjf4841@naver.com>
2024-12-04 22:18:24 +0900

    회원가입 응답 API 수정

---
+++ src/main/java/love/shop/web/login/dto/SignupResDto.java
@@ -0,0 +1,18 @@
+package love.shop.web.login.dto;
+
+import lombok.Data;
+import love.shop.domain.Member.Member;
+
+@Data
+public class SignupResDto {
+
+    private int status;
+    private String message;
+    private Member member;
+
+    public SignupResDto(int status, String message, Member member) {
+        this.status = status;
+        this.message = message;
+        this.member = member;
+    }
+}
