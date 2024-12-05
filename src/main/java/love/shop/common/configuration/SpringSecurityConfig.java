c6e423fb8096f3ef4fcc19c617c09f5198d5d4f5
hyc4841 <dbscjf4841@naver.com>
2024-12-05 17:32:26 +0900

    securyti 필터 수정

--- src/main/java/love/shop/common/configuration/SpringSecurityConfig.java
+++ src/main/java/love/shop/common/configuration/SpringSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,14 +26,18 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize ->
-
-                        authorize.anyRequest().permitAll())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션을 사용하지 않겠다는 설정
                 .formLogin(form -> form.disable())
-                .logout(logout -> logout.permitAll());
-
-        // 필터를 통해 토큰 기반 로그인
-        http.addFilterBefore(new JwtFilter(jwtTokenProvider, redisService), UsernamePasswordAuthenticationFilter.class);
+                .logout(logout -> logout.permitAll())
+                // 이 부분이 권한 관리 부분
+                .authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers("/signup", "login").anonymous() // /signup과 /login은 인증이 되지 않은 사용자의 접근을 허용한다는 의미. 즉 인증 없이 경로에 접근할 수 있음
+                                .requestMatchers("/api/**").hasRole("MEMBER") // /api/**로 들어오는 모든 요청은 MEMBER 권한이 있어야함
+                                .anyRequest().permitAll()
+                )
+                // 필터를 통해 토큰 기반 로그인
+                .addFilterBefore(new JwtFilter(jwtTokenProvider, redisService), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
