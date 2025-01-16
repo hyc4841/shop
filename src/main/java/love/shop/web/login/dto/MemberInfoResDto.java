b2686d787c1d0fa0b2dd30ca9ddd2c66cfd0b40e
hyc4841 <dbscjf4841@naver.com>
2025-01-16 18:24:49 +0900

    멤버 정보 응답 dto 수정

--- src/main/java/love/shop/web/login/dto/MemberInfoResDto.java
+++ src/main/java/love/shop/web/login/dto/MemberInfoResDto.java
@@ -1,19 +1,33 @@
 package love.shop.web.login.dto;
 
 import lombok.Data;
-import love.shop.domain.Member.Gender;
+import love.shop.domain.Address;
+import love.shop.domain.member.Gender;
+import love.shop.domain.member.MemberRole;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class MemberInfoResDto {
+    private String loginId;
     private String name;
-    private Integer age;
-    private String memo;
+    private LocalDate birthDate;
     private Gender gender;
+    private Address address;
+    private String email;
+    private LocalDate joinDate;
+    private List<MemberRole> memberRole;
 
-    public MemberInfoResDto(String name, Integer age, String memo, Gender gender) {
+
+    public MemberInfoResDto(String loginId, String name, LocalDate birthDate, Gender gender, Address address, String email, LocalDate joinDate, List<MemberRole> memberRole) {
+        this.loginId = loginId;
         this.name = name;
-        this.age = age;
-        this.memo = memo;
+        this.birthDate = birthDate;
         this.gender = gender;
+        this.address = address;
+        this.email = email;
+        this.joinDate = joinDate;
+        this.memberRole = memberRole;
     }
 }
