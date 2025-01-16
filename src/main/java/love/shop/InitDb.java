f5824289719e75c70ac80190fd5e26f24f1d6e5c
hyc4841 <dbscjf4841@naver.com>
2025-01-16 18:23:52 +0900

    테스트용 회원가입 수정

--- src/main/java/love/shop/InitDb.java
+++ src/main/java/love/shop/InitDb.java
@@ -3,12 +3,13 @@ package love.shop;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import love.shop.domain.Member.Gender;
-import love.shop.domain.Member.Member;
+import love.shop.domain.member.Gender;
 import love.shop.service.MemberService;
 import love.shop.web.signup.dto.SignupRequestDto;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Component
 @RequiredArgsConstructor
@@ -32,8 +33,10 @@ public class InitDb {
 
 
         public void dbInit() {
-            SignupRequestDto member = new SignupRequestDto("Hell4", 20, "이것은 테스트 입니다.", "1234", Gender.MAN);
-            memberService.signUp(member);
+            SignupRequestDto signupRequest = new SignupRequestDto("Hell4", "1234", "황윤철",
+                    LocalDate.of(1997, 6, 3), Gender.MAN, "서울", "서울시 은평구 백련산로 6 (응암동, 대주피오레아파트)", "33333", "101동 1103호","dbscjf4841@naver.com");
+
+            Long signUpMemberId = memberService.signUp(signupRequest);
         }
 
     }
