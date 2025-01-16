da7fe632ab4454c090d7bb5270a7671229ba0aa6
hyc4841 <dbscjf4841@naver.com>
2025-01-16 18:26:26 +0900

    회원가입 요청 dto 수정

--- src/main/java/love/shop/web/signup/dto/SignupRequestDto.java
+++ src/main/java/love/shop/web/signup/dto/SignupRequestDto.java
@@ -1,27 +1,44 @@
 package love.shop.web.signup.dto;
 
 import lombok.Data;
-import love.shop.domain.Member.Gender;
-import love.shop.domain.Member.Member;
+import love.shop.domain.Address;
+import love.shop.domain.member.Gender;
+import love.shop.domain.member.Member;
+
+import java.time.LocalDate;
 
 @Data
 public class SignupRequestDto {
 
-    private String name;
-    private Integer age;
-    private String memo;
+    private String loginId;
     private String password;
+    private String name;
+    private LocalDate birthDate;
     private Gender gender;
+    private String city;
+    private String street;
+    private String zipcode;
+    private String detailedAddress;
+    private String email;
 
-    public SignupRequestDto(String name, Integer age, String memo, String password, Gender gender) {
-        this.name = name;
-        this.age = age;
-        this.memo = memo;
+
+    public SignupRequestDto(String loginId, String password, String name, LocalDate birthDate, Gender gender, String city, String street, String zipcode, String detailedAddress, String email) {
+        this.loginId = loginId;
         this.password = password;
+        this.name = name;
+        this.birthDate = birthDate;
         this.gender = gender;
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+        this.detailedAddress = detailedAddress;
+        this.email = email;
     }
 
-    public Member toEntity(String name, Integer age, String memo, String password, Gender gender) {
-        return new Member(name, age, memo, password, gender);
+    public Member toMemberEntity(String loginId, String password, String name, LocalDate birthDate, Gender gender, String city, String street, String zipcode, String detailedAddress, String email) {
+        Address address = new Address(city, street, zipcode, detailedAddress);
+        return new Member(loginId, password, name, birthDate, gender, address, email, LocalDate.now());
     }
+
+
 }
