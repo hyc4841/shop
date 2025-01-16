019216b475aeb7d73be0c9d7f59c86b39ad15f6e
hyc4841 <dbscjf4841@naver.com>
2025-01-16 18:26:48 +0900

    패키지명 변경

--- src/main/java/love/shop/controller/TestApiController.java
+++ src/main/java/love/shop/controller/TestApiController.java
@@ -2,7 +2,7 @@ package love.shop.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import love.shop.domain.Member.Member;
+import love.shop.domain.member.Member;
 import love.shop.service.MemberService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
