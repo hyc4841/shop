6b060845aa95643aecc37f21129190c773df5284
hyc4841 <dbscjf4841@naver.com>
2025-01-19 18:59:20 +0900

    회원가입 성별 비교값 enum값에 맞춰 수정

--- src/main/java/love/shop/domain/Member/GenderValidator.java
+++ src/main/java/love/shop/domain/Member/GenderValidator.java
@@ -9,7 +9,8 @@ public class GenderValidator implements ConstraintValidator<ValidGender, Gender>
     @Override
     public boolean isValid(Gender gender, ConstraintValidatorContext context) {
         log.info("성별에 NONE 값이 들어오면 안됨");
-        return gender != null && !gender.equals("NONE");
+        log.info("gender 값={}", gender);
+        return gender != null && !gender.equals(Gender.NONE);
     }
 
 }
