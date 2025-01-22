24e5536be257ae94d8bcb92e75911b8ae513a0b0
hyc4841 <dbscjf4841@naver.com>
2025-01-23 00:00:03 +0900

    item 엔티티 설계

---
+++ src/main/java/love/shop/domain/item/Item.java
@@ -0,0 +1,19 @@
+package love.shop.domain.item;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+
+}
