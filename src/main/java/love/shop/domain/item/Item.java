24f8496561d87834861dfd63a60b001df9528581
hyc4841 <dbscjf4841@naver.com>
2025-01-23 16:50:54 +0900

    item 엔티티 업데이트

--- src/main/java/love/shop/domain/item/Item.java
+++ src/main/java/love/shop/domain/item/Item.java
@@ -1,19 +1,24 @@
 package love.shop.domain.item;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
 @Getter
 public class Item {
+
+    // 아이템은 일단 싱글 테이블로 만들고 추후 수정하는 방향으로 가자
 
     @Id
     @GeneratedValue
     @Column(name = "item_id")
     private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
 
 
 }
