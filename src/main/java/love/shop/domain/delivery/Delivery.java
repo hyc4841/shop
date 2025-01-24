e828c54eee18703f07864ce06c8de50dccc55aa0
hyc4841 <dbscjf4841@naver.com>
2025-01-25 01:24:55 +0900

    delivery setter 코드 추가

--- src/main/java/love/shop/domain/delivery/Delivery.java
+++ src/main/java/love/shop/domain/delivery/Delivery.java
@@ -28,4 +28,12 @@ public class Delivery {
     public void setOrder(Order order) {
         this.order = order;
     }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
 }
