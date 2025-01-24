2d845e2173db93dfc2eaf3cba9e3080f8a9f163c
hyc4841 <dbscjf4841@naver.com>
2025-01-25 01:23:28 +0900

    itemService 추가

---
+++ src/main/java/love/shop/service/item/ItemService.java
@@ -0,0 +1,32 @@
+package love.shop.service.item;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import love.shop.domain.item.Item;
+import love.shop.repository.item.ItemRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public Item findOne(Long ItemId) {
+        return itemRepository.findOne(ItemId);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+}
