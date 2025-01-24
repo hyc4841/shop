6968f08dd4ab049070ee219edc3d219c060dd410
hyc4841 <dbscjf4841@naver.com>
2025-01-25 01:21:39 +0900

    order 비즈니스 로직 추가

--- src/main/java/love/shop/domain/order/Order.java
+++ src/main/java/love/shop/domain/order/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import love.shop.domain.delivery.Delivery;
+import love.shop.domain.delivery.DeliveryStatus;
 import love.shop.domain.member.Member;
 import love.shop.domain.orderItem.OrderItem;
 
@@ -15,7 +16,7 @@ import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "orders") // order는 sql에서 예약어이므로 테이블 이름을 orders로 바꿔준다.
-@Getter @Setter
+@Getter
 public class Order {
 
     @Id
@@ -53,6 +54,49 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    // 주문 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // 비즈니스 로직
+    // 주문 취소
+    public void cancel() {
+        if (this.delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // 조회 로직
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getOrderPrice();
+        }
+        return totalPrice;
     }
 
 
