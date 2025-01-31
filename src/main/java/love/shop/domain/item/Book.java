c86a5a704d4679942838d7ccd424b4cf0f0c2f44
hyc4841 <dbscjf4841@naver.com>
2025-01-31 23:40:19 +0900

    Book 엔티티 추가

---
+++ src/main/java/love/shop/domain/item/Book.java
@@ -0,0 +1,21 @@
+package love.shop.domain.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("B")
+@Getter @Setter
+public class Book extends Item{
+
+    private String author;
+    private String isbn;
+
+    public Book(String bookName, int price, int quantity) {
+        super.setName(bookName);
+        super.setPrice(price);
+        super.setStockQuantity(quantity);
+    }
+}
