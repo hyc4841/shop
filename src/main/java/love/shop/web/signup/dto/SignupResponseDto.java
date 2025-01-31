866e3984756c097d7bffeb653eb5d878aa8a2723
hyc4841 <dbscjf4841@naver.com>
2025-01-31 23:39:50 +0900

    회원가입 응답 DTO 이름 수정

--- src/main/java/love/shop/web/signup/dto/SignupResponseDto.java
+++ src/main/java/love/shop/web/signup/dto/SignupResponseDto.java
@@ -4,13 +4,13 @@ import lombok.Data;
 import love.shop.domain.member.Member;
 
 @Data
-public class SignupResDto {
+public class SignupResponseDto {
 
     private int status;
     private String message;
     private Member member;
 
-    public SignupResDto(int status, String message, Member member) {
+    public SignupResponseDto(int status, String message, Member member) {
         this.status = status;
         this.message = message;
         this.member = member;
