33dd47e6d0eee2c42a4cec310320d745df089b5d
hyc4841 <dbscjf4841@naver.com>
2025-02-03 17:44:56 +0900

    엔티티 DTO 추가

---
+++ src/main/java/love/shop/web/order/dto/OrderDto.java
@@ -0,0 +1,35 @@
+package love.shop.web.order.dto;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import love.shop.domain.Address;
+import love.shop.domain.order.Order;
+import love.shop.domain.order.OrderStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Data
+public class OrderDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemDto> orderItems;
+
+    public OrderDto(Order order) {
+        this.orderId = order.getId();
+        this.name = order.getMember().getName();
+        this.orderDate = order.getOrderDate();
+        this.orderStatus = order.getStatus();
+        this.address = order.getDelivery().getAddress();
+        log.info("OrderItem 지연로딩 하나?");
+        this.orderItems = order.getOrderItems().stream()
+                .map(orderItem -> new OrderItemDto(orderItem))
+                .collect(Collectors.toList());
+    }
+}
