7f8287ef87be6c1fbbedea2b8f35b50f0db39d3c
hyc4841 <dbscjf4841@naver.com>
2025-02-10 17:42:41 +0900

    토큰 검증 예외 api 구조 변경

--- src/main/java/love/shop/common/exception/FilterExApi.java
+++ src/main/java/love/shop/common/exception/FilterExApi.java
@@ -2,25 +2,34 @@ package love.shop.common.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Slf4j
 @Component
 public class FilterExApi {
 
     // 필터에서 발생한 토큰 예외를 api로 처리하기 위한 함수
-    public void jwtTokenExHandler(HttpServletResponse response, String message, int code) throws IOException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    public void jwtTokenExHandler(HttpServletResponse response, String message, int code) {
+        response.setStatus(code);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8"); // 인코딩 안해주면 한글 안나옴
         try {
-            String api = new ObjectMapper().writeValueAsString(new ErrorApi(code, message));
+            ErrorApi errorApi = new ErrorApi(code, message);
+            // 에러 데이터를 한번 감싸서 보낸다.
+            String api = new ObjectMapper().writeValueAsString(new jwtExResult<>(errorApi));
             response.getWriter().write(api);
         } catch (Exception e) {
             log.error(e.getMessage());
         }
     }
+
+    @Data
+    @AllArgsConstructor
+    static class jwtExResult<T> {
+        private T error;
+    }
+
 }
