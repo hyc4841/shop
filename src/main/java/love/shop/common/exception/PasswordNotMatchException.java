cc1df3839fa63858e9efe808c14b6b68ba0255ae
hyc4841 <dbscjf4841@naver.com>
2025-02-12 20:19:48 +0900

    비밀번호 불일치 예외 추가

---
+++ src/main/java/love/shop/common/exception/PasswordNotMatchException.java
@@ -0,0 +1,25 @@
+package love.shop.common.exception;
+
+
+public class PasswordNotMatchException extends RuntimeException {
+
+    public PasswordNotMatchException() {
+        super();
+    }
+
+    public PasswordNotMatchException(String message) {
+        super(message);
+    }
+
+    public PasswordNotMatchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PasswordNotMatchException(Throwable cause) {
+        super(cause);
+    }
+
+    protected PasswordNotMatchException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
