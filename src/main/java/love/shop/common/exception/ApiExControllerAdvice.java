75ac13336fcecaee1ab0fac906ba586d9d126879
hyc4841 <dbscjf4841@naver.com>
2025-02-17 23:45:19 +0900

    멤버 정보 변경 예외 api 구조 변경

--- src/main/java/love/shop/common/exception/ApiExControllerAdvice.java
+++ src/main/java/love/shop/common/exception/ApiExControllerAdvice.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @RestControllerAdvice // RestControllerAdvice는 주로 컨트롤러에서 발생하는 예외를 처리해준다.
@@ -41,16 +40,35 @@ public class ApiExControllerAdvice {
 
     // 이게 지금 회원가입 필드 검증할때 뿐만 아니라 다른 곳에서 필드 검증할때도 사용되기 때문에 약간의 수정이 필요함.
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> fieldValidationExHandler(MethodArgumentNotValidException e, WebRequest request) {
+    public ResponseEntity<Map<String, List<ErrorMessages>>> fieldValidationExHandler(MethodArgumentNotValidException e, WebRequest request) {
         log.info("데이터 검증 실패");
-        HashMap<String, String> errors = new HashMap<>();
-        errors.put("message", "필드에 값을 정확히 입력해주세요.");
+        AtomicInteger i = new AtomicInteger(0); // 동시성 제어할 때 사용한다는데 잘 모르겠음
+        Map<String, List<ErrorMessages>> errors = new HashMap<>();
+        List<ErrorMessages> message = new ArrayList<>();
+        message.add(new ErrorMessages(1, "필드에 값을 정확히 입력해주세요."));
+        errors.put("message", message);
+
         // 아래와 같이 필요에 따라 필드 오류를 담아서 보낼 수 있음.
         e.getBindingResult().getFieldErrors().forEach(error -> {
             log.info("필드 이름={}, 에러 내용={}", error.getField(), error.getDefaultMessage());
-            errors.put(error.getField(), error.getDefaultMessage());
+
+            if (errors.get(error.getField()) == null) {
+                errors.put(error.getField(), new ArrayList<>());
+                errors.get(error.getField()).add(new ErrorMessages(errors.get(error.getField()).size() + 1, error.getDefaultMessage()));
+
+            } else {
+                errors.get(error.getField()).add(new ErrorMessages(errors.get(error.getField()).size() + 1, error.getDefaultMessage()));
+            }
         });
+
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class ErrorMessages {
+        private int id;
+        private String message;
     }
 
 
