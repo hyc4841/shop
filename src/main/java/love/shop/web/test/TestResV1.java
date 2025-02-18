38fcbb0949ea925b32e131f45ef3bc9b54d023ef
hyc4841 <dbscjf4841@naver.com>
2025-02-18 23:57:04 +0900

    api 테스트 코드 추가

---
+++ src/main/java/love/shop/web/test/TestResV1.java
@@ -0,0 +1,10 @@
+package love.shop.web.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TestResV1 {
+    private String response;
+}
---
+++ src/main/java/love/shop/web/test/TestResV2.java
@@ -0,0 +1,11 @@
+package love.shop.web.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TestResV2<T> {
+
+
+}
