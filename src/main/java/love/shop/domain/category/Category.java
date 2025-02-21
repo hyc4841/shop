79064fb8115128daae8be4158d0269462dcb1cd8
hyc4841 <dbscjf4841@naver.com>
2025-02-21 21:14:07 +0900

    Category 엔티티 ItemCategory 엔티티와 연관관계 추가

--- src/main/java/love/shop/domain/category/Category.java
+++ src/main/java/love/shop/domain/category/Category.java
@@ -3,6 +3,10 @@ package love.shop.domain.category;
 import jakarta.persistence.*;
 
 import lombok.Getter;
+import love.shop.domain.ItemCategory.ItemCategory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,8 +17,10 @@ public class Category {
     @Column(name = "category_id")
     private Long id;
 
+    // mappedBy = "category"는 주인쪽 category 필드를 참조하고 있다는 뜻
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<ItemCategory> itemCategories = new ArrayList<>();
+
     private String name;
-
-
 
 }
