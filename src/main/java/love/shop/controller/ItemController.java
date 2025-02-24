97148c6e08a0b7a7b32597112db2ce2cdfc568f2
hyc4841 <dbscjf4841@naver.com>
2025-02-25 00:28:03 +0900

    ItemController Item 저장 추가

--- src/main/java/love/shop/controller/ItemController.java
+++ src/main/java/love/shop/controller/ItemController.java
@@ -2,8 +2,14 @@ package love.shop.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import love.shop.domain.category.Category;
+import love.shop.domain.item.Book;
 import love.shop.service.item.ItemService;
+import love.shop.web.item.dto.BookSaveDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @Controller
@@ -12,5 +18,19 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    @GetMapping("/item")
+    public ResponseEntity<String> saveItem(@RequestBody BookSaveDto bookDto) {
+        // 그럼 책은 책대로, 전자제품은 전자제품대로 각자 다른 정보를 가지고 있어야 한다?
+        // 아이템을 저장할 때 필요한 것은 아이템 그자체와,
+
+        for (String categoryName : bookDto.getCategories()) {
+
+        }
+
+        Book book = new Book(bookDto.getAuthor(), bookDto.getIsbn(), bookDto.getName(), bookDto.getPrice(), bookDto.getStockQuantity());
+
+
+        return ResponseEntity.ok("ok");
+    }
 
 }
