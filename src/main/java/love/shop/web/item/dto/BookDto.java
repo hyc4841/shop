196d6c21efc90a63a526fbc9020a0f7b06619ce7
hyc4841 <dbscjf4841@naver.com>
2025-02-26 00:04:39 +0900

    BookDto 추가

---
+++ src/main/java/love/shop/web/item/dto/BookDto.java
@@ -0,0 +1,31 @@
+package love.shop.web.item.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import love.shop.domain.item.Book;
+
+import java.util.stream.Collectors;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class BookDto extends ItemDto {
+
+    private String author;
+    private String isbn;
+
+    private String dtyep = "Book";
+
+    public BookDto(Book book) {
+        super.setName(book.getName());
+        super.setPrice(book.getPrice());
+        super.setStockQuantity(book.getStockQuantity());
+        super.setItemCategories(book.getItemCategories().stream()
+                .map(itemCategory -> new ItemCategoryDto(itemCategory))
+                .collect(Collectors.toList()));
+
+        this.author = book.getAuthor();
+        this.isbn = book.getIsbn();
+
+    }
+
+}
