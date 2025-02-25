4eaaabd885a8db01445a46b9937f300dd656ae09
hyc4841 <dbscjf4841@naver.com>
2025-02-26 00:03:00 +0900

    ItemCategoryDto 추가

---
+++ src/main/java/love/shop/web/item/dto/ItemCategoryDto.java
@@ -0,0 +1,13 @@
+package love.shop.web.item.dto;
+
+import lombok.Data;
+import love.shop.domain.ItemCategory.ItemCategory;
+
+@Data
+public class ItemCategoryDto {
+    private String categoryName;
+
+    public ItemCategoryDto(ItemCategory itemCategory) {
+        this.categoryName = itemCategory.getCategory().getName();
+    }
+}
