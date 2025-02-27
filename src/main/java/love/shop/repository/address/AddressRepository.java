ec3dc27819c33a56a1eaae5e2088cb269116f46c
hyc4841 <dbscjf4841@naver.com>
2025-02-27 19:47:41 +0900

    AddressRepository 주소 조회 추가

--- src/main/java/love/shop/repository/address/AddressRepository.java
+++ src/main/java/love/shop/repository/address/AddressRepository.java
@@ -1,5 +1,7 @@
-package love.shop.repository.Address;
+package love.shop.repository.address;
 
+import com.querydsl.core.QueryFactory;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,8 +14,15 @@ import org.springframework.stereotype.Controller;
 public class AddressRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
 
+    // 주소 저장
     public void save(Address address) {
         em.persist(address);
+    }
+
+    // 주소 조회
+    public Address findAddressById(Long id) {
+        return em.find(Address.class, id);
     }
 }
