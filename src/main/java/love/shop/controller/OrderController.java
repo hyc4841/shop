1fabca03e070171c448ec9ca49def49fecf17b9a
hyc4841 <dbscjf4841@naver.com>
2025-02-28 23:51:54 +0900

    OrderController 멤버 주문 조회, 주문 저장 컨트롤러 추가

--- src/main/java/love/shop/controller/OrderController.java
+++ src/main/java/love/shop/controller/OrderController.java
@@ -4,15 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import love.shop.domain.item.Item;
+import love.shop.domain.member.Member;
 import love.shop.domain.order.Order;
+import love.shop.domain.orderItem.OrderItem;
+import love.shop.repository.item.ItemRepository;
 import love.shop.repository.order.OrderRepository;
+import love.shop.service.member.MemberService;
 import love.shop.service.order.OrderService;
+import love.shop.web.login.dto.CustomUser;
 import love.shop.web.order.dto.OrderDto;
+import love.shop.web.order.dto.OrderReqDto;
 import love.shop.web.order.dto.OrdersResponseDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +31,8 @@ public class OrderController {
 
     private final OrderService orderService;
     private final OrderRepository orderRepository;
+    private final MemberService memberService;
+    private final ItemRepository itemRepository;
 
     //    @GetMapping("/orders")
     public ResponseEntity<AllOrdersResult<List<OrdersResponseDto>>> findAllOrders() {
@@ -37,39 +45,53 @@ public class OrderController {
         return ResponseEntity.ok(new AllOrdersResult<List<OrdersResponseDto>>(result, result.size()));
     }
 
+    // 멤버 주문 조회
     @GetMapping("/orders")
-    public ResponseEntity<AllOrders<List<OrderDto>>> findAllOrders(@RequestParam(value = "offset", defaultValue = "0") int offset,
-                                                                   @RequestParam(value = "limit", defaultValue = "100") int limit) {
+    public ResponseEntity<AllOrders<List<OrderDto>>> findOrdersByMember(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                                                        @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        Long memberId = ((CustomUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getMemberId(); // jwt 토큰으로 부터 멤버 정보 가져오기
 
-        log.info("여기서 일단 fetch join으로 가져오고.");
-        List<Order> orders = orderService.findAllOrders(offset, limit);
+        List<Order> orders = orderService.findOrdersByMemberId(memberId, offset, limit);
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
         return ResponseEntity.ok(new AllOrders<List<OrderDto>>(result, result.size()));
     }
 
-    // 이거 고급 기술임. 만약에 클라이언트에서 모든과 주문 개수를 요청했을 때 그냥 DTO로 넘기면 배열 값으로 바로 넘어가기 때문에 필드 확장이 불가능함.
-    // 클레스로 컬렉션을 감싸면 나중에 필요한 필드를 추가할 수 있음
-    @Data
-    @AllArgsConstructor
-    static class AllOrdersResult<T> {
-        private T data; // 주문 컬렉션. 주문 리스트
-        private int count; // 주문 개수
+    @PostMapping("/order")
+    public ResponseEntity saveOrder(@RequestBody OrderReqDto orderReqDto) {
+
+        // 주문을 만들기 위해서 뭐가 필요하지? 멤버, 아이템, 배송정보가 필요함
+        Long memberId = ((CustomUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getMemberId(); // jwt 토큰으로 부터 멤버 정보 가져오기
+
+        orderService.order(memberId, orderReqDto.getItemId(), orderReqDto.getCount(), orderReqDto.getAddressId(),
+                );
+
+
     }
 
-    @Data
-    @AllArgsConstructor
-    static class AllOrders<T> {
-        private T orders;
-        private int count;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     @GetMapping("/orders/test1")
     public ResponseEntity<List<OrderDto>> findOrdersByMemberId() {
         Long memberId = 1L;
-        List<Order> orders = orderRepository.findOrdersByMemberId(1L);
+        List<Order> orders = orderRepository.findOrdersByMemberId(1L, 0, 100);
 
         List<OrderDto> orderDtos = new ArrayList<>();
 
@@ -99,7 +121,7 @@ public class OrderController {
     @GetMapping("/orders/test3")
     public ResponseEntity<List<OrderDto>> findOrdersByMember123() {
 
-        List<Order> orders = orderRepository.findAllOrdersV2(0, 100);
+        List<Order> orders = orderRepository.findAllOrders(0, 100);
 
         List<OrderDto> list = new ArrayList<>();
 
@@ -113,7 +135,21 @@ public class OrderController {
 
 
 
+    // 이거 고급 기술임. 만약에 클라이언트에서 모든과 주문 개수를 요청했을 때 그냥 DTO로 넘기면 배열 값으로 바로 넘어가기 때문에 필드 확장이 불가능함.
+    // 클레스로 컬렉션을 감싸면 나중에 필요한 필드를 추가할 수 있음
+    @Data
+    @AllArgsConstructor
+    static class AllOrdersResult<T> {
+        private T data; // 주문 컬렉션. 주문 리스트
+        private int count; // 주문 개수
+    }
 
+    @Data
+    @AllArgsConstructor
+    static class AllOrders<T> {
+        private T orders;
+        private int count;
+    }
 
 
 
