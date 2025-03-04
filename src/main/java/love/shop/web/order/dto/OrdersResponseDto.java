cae61aec115a937306c67ab90384455458619976
hyc4841 <dbscjf4841@naver.com>
2025-03-04 23:38:02 +0900

    OrdersResponseDto 주소 부분 삭제

--- src/main/java/love/shop/web/order/dto/OrdersResponseDto.java
+++ src/main/java/love/shop/web/order/dto/OrdersResponseDto.java
@@ -13,20 +13,13 @@ public class OrdersResponseDto {
     private String memberName;
     private LocalDateTime orderDate;
     private OrderStatus orderStatus;
-    private String city;
-    private String street;
-    private String detailedAddress;
-    private String zipcode;
 
     public OrdersResponseDto(Order order) {
         this.orderId = order.getId();
         this.memberName = order.getMember().getName();
         this.orderDate = order.getOrderDate();
         this.orderStatus = order.getStatus();
-        this.city = order.getDelivery().getCity();
-        this.street = order.getDelivery().getStreet();
-        this.detailedAddress = order.getDelivery().getDetailedAddress();
-        this.zipcode = order.getDelivery().getZipcode();
+
     }
 
 }
