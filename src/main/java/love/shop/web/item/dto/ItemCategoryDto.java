e9505879daff4dd4424cbaec0817d8cc02e15a78
hyc4841 <dbscjf4841@naver.com>
2025-03-06 22:12:05 +0900

    ItemCategoryDto 생성자 추가

--- src/main/java/love/shop/web/item/dto/ItemCategoryDto.java
+++ src/main/java/love/shop/web/item/dto/ItemCategoryDto.java
@@ -6,8 +6,16 @@ import love.shop.domain.ItemCategory.ItemCategory;
 @Data
 public class ItemCategoryDto {
     private String categoryName;
+    private String itemName;
 
+    /*
     public ItemCategoryDto(ItemCategory itemCategory) {
         this.categoryName = itemCategory.getCategory().getCategoryName();
+    }
+     */
+
+    // 카테고리 입장에서 해당 카테고리에 어떤 아이템이 있는지 볼 수 있음
+    public ItemCategoryDto(ItemCategory itemCategory) {
+        this.itemName = itemCategory.getItem().getName();
     }
 }
