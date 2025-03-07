a08d8b765e99a02da0a67d0294e504d04e06718a
hyc4841 <dbscjf4841@naver.com>
2025-03-07 16:37:25 +0900

    ItemController 조회 메서드 이름 변경

--- src/main/java/love/shop/controller/ItemController.java
+++ src/main/java/love/shop/controller/ItemController.java
@@ -3,7 +3,7 @@ package love.shop.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import love.shop.domain.category.Category;
-import love.shop.domain.item.Book;
+import love.shop.domain.item.type.Book;
 import love.shop.domain.item.Item;
 import love.shop.repository.item.ItemRepository;
 import love.shop.service.item.ItemService;
@@ -24,13 +24,10 @@ public class ItemController {
     private final ItemService itemService;
     private final ItemRepository itemRepository;
 
-    @GetMapping("/item")
-    public ResponseEntity<String> saveItem(@RequestBody BookSaveReqDto bookDto) {
-        // 그럼 책은 책대로, 전자제품은 전자제품대로 각자 다른 정보를 가지고 있어야 한다?
-        // 아이템을 저장할 때 필요한 것은 아이템 그자체와
+    @PostMapping("/item")
+    public ResponseEntity<String> saveItemWithCategory(@RequestBody ItemSaveReqDto itemDto) {
 
 
-        // 일대다, 다대일, 객체 생성하고 언제 어디서 넣어줘야하는지, 특히 Many 애들 어떻게 해줘야하는지
         // 지금은 뭘하고 있냐면 아이템 생성하고 저장하려고 하는데 아이템-카테고리 아이템 생성할 때 같이 저장하고 싶은데 어떻게 할지
         // 헤매고 있는중임.
         // 상속 관계일 때는 객체 어떻게 생성하는지도 확실히 알아야함.
