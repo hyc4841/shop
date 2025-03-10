a34d50168d0f4781bf70127c98e36a57736ec332
hyc4841 <dbscjf4841@naver.com>
2025-03-10 23:29:54 +0900

    ItemService saveItemWithCategory 반환값 Item 추가

--- src/main/java/love/shop/service/item/ItemService.java
+++ src/main/java/love/shop/service/item/ItemService.java
@@ -7,8 +7,8 @@ import love.shop.domain.category.Category;
 import love.shop.domain.item.type.Book;
 import love.shop.domain.item.Item;
 import love.shop.repository.item.ItemRepository;
-import love.shop.web.item.dto.BookUpdateReqDto;
-import love.shop.web.item.dto.ItemSaveReqDto;
+import love.shop.web.item.updateDto.BookUpdateReqDto;
+import love.shop.web.item.saveDto.ItemSaveReqDto;
 import love.shop.web.item.dto.SearchCond;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,24 +29,25 @@ public class ItemService {
         itemRepository.save(item);
 
         // 보통 아이템을 저장할 땐 카테고리를 같이 넣어서 저장한다.
-
     }
 
     @Transactional
-    public void saveItemWithCategory(ItemSaveReqDto itemDto, List<Integer> categoriesId) {
+    public Item saveItemWithCategory(ItemSaveReqDto itemDto) {
+
         Item item = Item.createItem(itemDto);// 아이템 저장 요청으로 들어온 DTO를 각 데이터타입에 맞게 구분한 후 해당 데이터타입의
 
-        // 아이템에 카테고리 등록
-        for (Integer categoryId : categoriesId) {
-            Category category = itemRepository.findCategoryById(Long.valueOf(categoryId)).orElseThrow(() -> new RuntimeException("해당 카테고리가 없음"));
-            ItemCategory itemCategory = ItemCategory.createItemCategory(category);
-            item.addItemCategory(itemCategory);
+        if (item != null) {
+            // 아이템에 카테고리 등록
+            for (Integer categoryId : itemDto.getCategoriesId()) {
+                Category category = itemRepository.findCategoryById(Long.valueOf(categoryId)).orElseThrow(() -> new RuntimeException("해당 카테고리가 없음"));
+                ItemCategory itemCategory = ItemCategory.createItemCategory(category);
+                item.addItemCategory(itemCategory);
+            }
         }
-
         save(item);
+
+        return item;
     }
-
-
 
     // 아이템 id로 조회
     public Item findOne(Long ItemId) {
