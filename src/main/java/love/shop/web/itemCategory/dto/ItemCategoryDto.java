65b01a62ebe53515c2bea357a49de71906d202d8
hyc4841 <dbscjf4841@naver.com>
2025-03-19 19:52:02 +0900

    ItemCategoryDto categoryName 도 포함하도록 변경

--- src/main/java/love/shop/web/itemCategory/dto/ItemCategoryDto.java
+++ src/main/java/love/shop/web/itemCategory/dto/ItemCategoryDto.java
@@ -17,5 +17,6 @@ public class ItemCategoryDto {
     // 카테고리 입장에서 해당 카테고리에 어떤 아이템이 있는지 볼 수 있음
     public ItemCategoryDto(ItemCategory itemCategory) {
         this.itemName = itemCategory.getItem().getName();
+        this.categoryName = itemCategory.getCategory().getCategoryName();
     }
 }
