4748e7479c1afd31829b56d8e0ec476cf2316d0f
hyc4841 <dbscjf4841@naver.com>
2025-03-19 19:46:58 +0900

    LapTopSearchFilter 생성. 노트북 필터

---
+++ src/main/java/love/shop/web/item/searchFilter/LapTopSearchFilter.java
@@ -0,0 +1,42 @@
+package love.shop.web.item.searchFilter;
+
+import lombok.Data;
+import love.shop.web.item.filter.lapTop.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+public class LapTopSearchFilter {
+
+    private List<LapTopBrand> lapTopBrands;
+    private List<LapTopCpu> lapTopCpus;
+    private List<LapTopStorage> lapTopStorages;
+    private List<LapTopScreenSize> lapTopScreenSizes;
+    private List<LapTopManufactureBrand> lapTopManufactureBrands;
+
+    public LapTopSearchFilter(List<LapTopBrand> lapTopBrands, List<LapTopCpu> lapTopCpus, List<LapTopStorage> lapTopStorages, List<LapTopScreenSize> lapTopScreenSizes, List<LapTopManufactureBrand> lapTopManufactureBrands) {
+        this.lapTopBrands = lapTopBrands;
+        this.lapTopCpus = lapTopCpus;
+        this.lapTopStorages = lapTopStorages;
+        this.lapTopScreenSizes = lapTopScreenSizes;
+        this.lapTopManufactureBrands = lapTopManufactureBrands;
+    }
+
+    public static LapTopSearchFilter createLapTopFilter() {
+        LapTopSearchFilter lapTopSearchFilter = new LapTopSearchFilter();
+        lapTopSearchFilter.setLapTopBrands(Arrays.asList(LapTopBrand.values()));
+        lapTopSearchFilter.setLapTopCpus(Arrays.asList(LapTopCpu.values()));
+        lapTopSearchFilter.setLapTopStorages(Arrays.asList(LapTopStorage.values()));
+        lapTopSearchFilter.setLapTopScreenSizes(Arrays.asList(LapTopScreenSize.values()));
+        lapTopSearchFilter.setLapTopManufactureBrands(Arrays.asList(LapTopManufactureBrand.values()));
+
+        return lapTopSearchFilter;
+    }
+
+
+
+    public LapTopSearchFilter() {
+
+    }
+}
