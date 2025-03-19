a9b9fffe5d5aa53e722ea5c08e92d8063287e63c
hyc4841 <dbscjf4841@naver.com>
2025-03-19 19:49:13 +0900

    LapTopScreenSize String 추가

--- src/main/java/love/shop/web/item/filter/lapTop/LapTopScreenSize.java
+++ src/main/java/love/shop/web/item/filter/lapTop/LapTopScreenSize.java
@@ -1,5 +1,18 @@
 package love.shop.web.item.filter.lapTop;
 
 public enum LapTopScreenSize {
-    Inch_17, Inch_16, Inch_15, Inch_13
+    Inch_17("17인치"), Inch_16("16인치"), Inch_15("15인치"), Inch_13("13인치");
+
+    private final String screenSize;
+
+
+    LapTopScreenSize(String screenSize) {
+        this.screenSize = screenSize;
+    }
+
+
+    @Override
+    public String toString() {
+        return screenSize;
+    }
 }
