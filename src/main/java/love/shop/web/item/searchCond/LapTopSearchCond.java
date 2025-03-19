27dd246ef681e69d30d69abaa2299c87d9a2b11b
hyc4841 <dbscjf4841@naver.com>
2025-03-19 19:48:33 +0900

    LapTopSearchCond 실험적으로 각종 생성자 및 게터 세터 추가

--- src/main/java/love/shop/web/item/searchCond/LapTopSearchCond.java
+++ src/main/java/love/shop/web/item/searchCond/LapTopSearchCond.java
@@ -1,17 +1,32 @@
 package love.shop.web.item.searchCond;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
+import love.shop.web.item.filter.lapTop.*;
 
-@JsonTypeName(value = "LapTop")
+import java.util.List;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class LapTopSearchCond extends SearchCond{
+@JsonTypeName(value = "LapTop")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LapTopSearchCond extends SearchCond {
 
-    private String lapTopBrand;
-    private String lapTopCpu;
-    private String lapTopStorage;
-    private String lapTopScreenSize;
-    private String lapTopManufactureBrand;
+    private List<LapTopBrand> lapTopBrands;
+    private List<LapTopCpu> lapTopCpus;
+    private List<LapTopStorage> lapTopStorages;
+    private List<LapTopScreenSize> lapTopScreenSizes;
+    private List<LapTopManufactureBrand> lapTopManufactureBrands;
+
+    public LapTopSearchCond(String type, Long categories, String itemName, Integer morePrice, Integer lessPrice, List<LapTopBrand> lapTopBrands, List<LapTopCpu> lapTopCpus, List<LapTopStorage> lapTopStorages, List<LapTopScreenSize> lapTopScreenSizes, List<LapTopManufactureBrand> lapTopManufactureBrands) {
+        super(type, categories, itemName, morePrice, lessPrice);
+        this.lapTopBrands = lapTopBrands;
+        this.lapTopCpus = lapTopCpus;
+        this.lapTopStorages = lapTopStorages;
+        this.lapTopScreenSizes = lapTopScreenSizes;
+        this.lapTopManufactureBrands = lapTopManufactureBrands;
+    }
 }
