b171516dfce28ad42345aa625d5fd130c04c2b2b
hyc4841 <dbscjf4841@naver.com>
2025-03-26 01:33:56 +0900

    ItemCategoryDto 필요 없는 코드 삭제

--- src/main/java/love/shop/web/itemCategory/dto/ItemCategoryDto.java
+++ src/main/java/love/shop/web/itemCategory/dto/ItemCategoryDto.java
@@ -8,12 +8,6 @@ public class ItemCategoryDto {
     private String categoryName;
     private String itemName;
 
-    /*
-    public ItemCategoryDto(ItemCategory itemCategory) {
-        this.categoryName = itemCategory.getCategory().getCategoryName();
-    }
-     */
-
     // 카테고리 입장에서 해당 카테고리에 어떤 아이템이 있는지 볼 수 있음
     public ItemCategoryDto(ItemCategory itemCategory) {
         this.itemName = itemCategory.getItem().getName();
