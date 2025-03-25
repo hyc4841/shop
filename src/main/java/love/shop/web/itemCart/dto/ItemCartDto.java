e305678aacd06c77aaa737134f7078ba033d6100
hyc4841 <dbscjf4841@naver.com>
2025-03-26 01:31:58 +0900

    ItemCartDto 생성

---
+++ src/main/java/love/shop/web/itemCart/dto/ItemCartDto.java
@@ -0,0 +1,19 @@
+package love.shop.web.itemCart.dto;
+
+import lombok.Data;
+import love.shop.domain.itemCart.ItemCart;
+import love.shop.web.item.dto.ItemDto;
+
+@Data
+public class ItemCartDto {
+
+    private Long id;
+    private ItemDto item;
+    private Integer itemCount;
+
+    public ItemCartDto(ItemCart itemCart) {
+        this.id = itemCart.getId();
+        this.item = ItemDto.createItemDto(itemCart.getItem()); // 걸리는 부분은 이부분
+        this.itemCount = itemCart.getItemCount();
+    }
+}
