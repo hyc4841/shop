7f02c650bf2f6b00950d32b1d0a9de5ef3dc990e
hyc4841 <dbscjf4841@naver.com>
2025-03-30 22:03:54 +0900

    OrderUpdateDto -> OrderDeliveryAddressUpdateDto 이름 변경

--- src/main/java/love/shop/web/order/dto/OrderDeliveryAddressUpdateDto.java
+++ src/main/java/love/shop/web/order/dto/OrderDeliveryAddressUpdateDto.java
@@ -1,15 +1,13 @@
 package love.shop.web.order.dto;
 
 import lombok.Data;
-import love.shop.domain.delivery.DeliveryStatus;
 
 @Data
-public class OrderUpdateDto {
+public class OrderDeliveryAddressUpdateDto {
 
-    private DeliveryStatus deliveryStatus; // 배송상태
+    private Long addressId; // 주문자가 미리 저장한 주소를 선택한 경우
 
-    private Long addressId;
-
+    // 직접 입력한 경우
     private String city;
     private String street;
     private String zipcode;
