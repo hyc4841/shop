e921c852cb45c772fa278fc895ca2f32bad3b46b
hyc4841 <dbscjf4841@naver.com>
2025-03-31 17:45:24 +0900

    Page 생성

---
+++ src/main/java/love/shop/domain/page/Page.java
@@ -0,0 +1,42 @@
+package love.shop.domain.page;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import love.shop.domain.itemPage.ItemPage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Page {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "page_id")
+    private Long id;
+
+    private String pageName;        // 상품 페이지 이름
+    private List<String> images;    // 상품 페이지 이미지
+    private String description;     // 상품 페이지 설명
+
+    @OneToMany(mappedBy = "page")
+    private List<ItemPage> itemPages = new ArrayList<>();
+//    private String seller;          // 판매자
+
+
+    public Page(String pageName, List<String> images, String description) {
+        this.pageName = pageName;
+        this.images = images;
+        this.description = description;
+    }
+
+
+    public void setItemPages(ItemPage itemPages) {
+        this.itemPages.add(itemPages);
+    }
+}
