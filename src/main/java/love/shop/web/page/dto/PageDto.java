fc7725c8af6f91d3ef26cd0e11e2021283bba23f
hyc4841 <dbscjf4841@naver.com>
2025-03-31 17:45:49 +0900

    PageDto 생성

---
+++ src/main/java/love/shop/web/page/dto/PageDto.java
@@ -0,0 +1,44 @@
+package love.shop.web.page.dto;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import love.shop.domain.page.Page;
+import love.shop.web.itemPage.dto.ItemPageDto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Data
+public class PageDto {
+
+    private Long id;
+    private String pageName;
+    private List<String> images;
+    private String description;
+
+    private List<ItemPageDto> itemPages;
+
+    public PageDto(Page page) {
+        this.id = page.getId();
+        this.pageName = page.getPageName();
+        this.images = page.getImages();
+        this.description = page.getDescription();
+
+        this.itemPages = page.getItemPages().stream()
+                .map(itemPage -> new ItemPageDto(itemPage))
+                .collect(Collectors.toList());
+    }
+
+    public static List<PageDto> createPageDtoList(List<Page> pageList) {
+        List<PageDto> pageDtoList = new ArrayList<>();
+
+        for (Page page : pageList) {
+            log.info("지연조회 시점");
+            pageDtoList.add(new PageDto(page));
+        }
+
+        return pageDtoList;
+    }
+}
