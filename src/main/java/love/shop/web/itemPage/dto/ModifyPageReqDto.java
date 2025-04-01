f16588b5cc6abc3603e5da5bba381df29aac8778
hyc4841 <dbscjf4841@naver.com>
2025-04-01 18:17:00 +0900

    ModifyPageReqDto 페이지 수정 요청 dto 추가

---
+++ src/main/java/love/shop/web/itemPage/dto/ModifyPageReqDto.java
@@ -0,0 +1,20 @@
+package love.shop.web.itemPage.dto;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class ModifyPageReqDto {
+
+    private Long pageId;
+
+    private Map<String, List<Long>> optionAndItem; // 새로 넣을 아이템
+    private List<Long> deleteItems; // 제거할 아이템 ItemPage의 id임.
+
+    private String pageName;
+    private List<String> images;
+    private String description;
+
+}
