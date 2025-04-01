564198ba35ef873c4ca3cb8bdfe56d6f01bffd25
hyc4841 <dbscjf4841@naver.com>
2025-04-01 18:18:52 +0900

    ItemPage sequence 필드 추가 및 item, page 연관관계 제거 메서드 추가

--- src/main/java/love/shop/domain/itemPage/ItemPage.java
+++ src/main/java/love/shop/domain/itemPage/ItemPage.java
@@ -29,6 +29,9 @@ public class ItemPage {
     @Column
     private String optionName; // 해당 페이지에 어떤 옵션의 이름으로 들어 갈건지
 
+    @Column
+    private Integer sequence; // 목록에서 보일 순서
+
     // 그럼 이제 조금 달라짐. 기존에는 아이템을 검색하는 거였다면 이젠 아이템 페이지를 검색하는 꼴이됨.
     // 페이지를 직접 검색하기 보단. 어떤 아이템을 가진 itemPage를 검색해서 itemPage가 가지고 있는 페이지를 가져오면 될듯.
 
@@ -46,6 +49,14 @@ public class ItemPage {
     public void setPage(Page page) {
         this.page = page;
         page.setItemPages(this);
+    }
+
+    // page와 item 연관관계 끊어주기
+    public void deleteItemPage() {
+        this.page.getItemPages().remove(this);
+        this.page = null;
+        this.item.getItemPages().remove(this);
+        this.item = null;
     }
 
 }
