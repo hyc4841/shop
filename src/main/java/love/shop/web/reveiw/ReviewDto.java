f45329ab2018f7a37b979dbb4d0dbb5b3aade2ac
hyc4841 <dbscjf4841@naver.com>
2025-04-02 11:13:27 +0900

    ReviewDto 생성

---
+++ src/main/java/love/shop/web/reveiw/ReviewDto.java
@@ -0,0 +1,36 @@
+package love.shop.web.reveiw;
+
+import lombok.Data;
+import love.shop.domain.review.Review;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class ReviewDto {
+
+    private Long id;
+
+    private Long memberId;
+    private Long orderId;
+    private Long pageId;
+    private String content;
+    private List<String> images;
+    private LocalDateTime createAt;
+    private Integer likes;
+    private Integer starRating;
+
+    public ReviewDto(Review review) {
+        this.id = review.getId();
+        this.memberId = review.getMember().getId();
+        this.orderId = review.getOrder().getId();
+        this.pageId = review.getPage().getId();
+        this.content = review.getContent();
+        this.images = review.getImages();
+        this.createAt = review.getCreatedAt();
+        this.likes = review.getLikes();
+        this.starRating = review.getStarRating();
+
+    }
+
+}
