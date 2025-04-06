88c08e27c3c95066c8c720c31f2b5ffc56e75d73
hyc4841 <dbscjf4841@naver.com>
2025-04-07 01:02:10 +0900

    아이디 공백 검사 추가

---
+++ src/main/java/love/shop/domain/Member/validator/ValidLoginIdBlank.java
@@ -0,0 +1,18 @@
+package love.shop.domain.member.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = LoginIdBlankValidator.class)
+public @interface ValidLoginIdBlank {
+    String message() default "아이디에는 공백이 포함될 수 없습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
