149f741ace58d265fdb0e5d5e58bcee610fec0db
hyc4841 <dbscjf4841@naver.com>
2025-04-07 01:02:00 +0900

    이메일 공백 검사 추가

---
+++ src/main/java/love/shop/domain/Member/validator/EmailBlankValidator.java
@@ -0,0 +1,20 @@
+package love.shop.domain.member.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class EmailBlankValidator implements ConstraintValidator<ValidEmailBlank, String> {
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        boolean matches = email.matches(".*\\s+.*");// 정규 표현식으로 공백 포함되어 있는지 검사
+
+        return !matches;
+    }
+}
