984b28136b7febe16e7e706aaa54644748308881
hyc4841 <dbscjf4841@naver.com>
2025-04-10 23:10:43 +0900

    EmailCertificationDto 필드 검증 추가

--- src/main/java/love/shop/web/signup/dto/EmailCertificationDto.java
+++ src/main/java/love/shop/web/signup/dto/EmailCertificationDto.java
@@ -1,10 +1,15 @@
 package love.shop.web.signup.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class EmailCertificationDto {
 
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+//    @ValidEmailDuplication 이거 다시 원상 복구 시켜야함. 지금은 테스트라 주석처리함
     private String email;
 
 }
