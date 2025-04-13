f7008a99e6ad8639a63a0114cc43ff94b01f64a5
hyc4841 <dbscjf4841@naver.com>
2025-04-13 22:37:09 +0900

    코드 정리

--- src/main/java/love/shop/web/item/saveDto/BookSaveReqDto.java
+++ src/main/java/love/shop/web/item/saveDto/BookSaveReqDto.java
@@ -1,10 +1,12 @@
 package love.shop.web.item.saveDto;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
+@JsonTypeName(value = "Book")
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class BookSaveReqDto extends ItemSaveReqDto {
