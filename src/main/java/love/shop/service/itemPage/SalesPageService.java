fe219257f7dd404ea70ae585faccf49c173bb02c
hyc4841 <dbscjf4841@naver.com>
2025-04-13 22:43:38 +0900

    SalesPageService 판매 페이지 저장 dto 데이터 구조 변경에 따른 코드 변경

--- src/main/java/love/shop/service/itemPage/SalesPageService.java
+++ src/main/java/love/shop/service/itemPage/SalesPageService.java
@@ -7,6 +7,7 @@ import love.shop.domain.itemSalesPage.ItemSalesPage;
 import love.shop.domain.salesPage.SalesPage;
 import love.shop.repository.ItemPage.SalesPageRepository;
 import love.shop.repository.item.ItemRepository;
+import love.shop.web.item.searchCond.SearchCond;
 import love.shop.web.itemPage.dto.CreatePageReqDto;
 import love.shop.web.page.dto.SalesPageDto;
 import org.springframework.stereotype.Service;
@@ -21,21 +22,21 @@ import java.util.Map;
 @Transactional(readOnly = true)
 public class SalesPageService {
 
-    private final SalesPageRepository itemPageRepository;
+    private final SalesPageRepository salesPageRepository;
     private final ItemRepository itemRepository;
 
     @Transactional
     public SalesPage modifyItemPage(Long pageId, List<Long> deleteItems, String pageName,
-                                    List<String> images, String description, Map<String, List<Long>> optionAndItem) {
+                                    List<String> images, String description, Map<String, Map<Long, Map<String, Boolean>>> optionAndItem) {
 
-        SalesPage page = itemPageRepository.findPageByPageId(pageId).orElseThrow(() -> new RuntimeException());
+        SalesPage page = salesPageRepository.findPageByPageId(pageId).orElseThrow(() -> new RuntimeException());
 
         // 1. 기존에 등록되어 있는 아이템 삭제할거 있으면 삭제함
         if (deleteItems != null) {
             for (Long itemPageId : deleteItems) {
-                ItemSalesPage itemPage = itemPageRepository.findItemPageByItemPageId(itemPageId).orElseThrow(() -> new RuntimeException());
+                ItemSalesPage itemPage = salesPageRepository.findItemPageByItemPageId(itemPageId).orElseThrow(() -> new RuntimeException());
                 itemPage.deleteItemPage();
-                itemPageRepository.deleteItemPage(itemPageId);
+                salesPageRepository.deleteItemPage(itemPageId);
             }
         }
 
@@ -45,7 +46,7 @@ public class SalesPageService {
         // 3. 새로 등록할 아이템 등록하기
         itemPageReg(page, optionAndItem);
 
-        itemPageRepository.savePage(page);
+        salesPageRepository.savePage(page);
 
         return page;
     }
@@ -57,7 +58,7 @@ public class SalesPageService {
 
         itemPageReg(page, pageReqDto.getOptionAndItem());
 
-        itemPageRepository.savePage(page);
+        salesPageRepository.savePage(page);
 
         return page;
     }
@@ -65,19 +66,47 @@ public class SalesPageService {
     // 아이템의 카테고리로 아이템 페이지 찾기
     public List<SalesPageDto> findItemPageWithItemCategory(Long categoryId, int offset, int limit) {
 
-        List<SalesPage> pageByItemCategory = itemPageRepository.findPageByItemCategory(categoryId, offset, limit);
+        List<SalesPage> pageByItemCategory = salesPageRepository.findPageByItemCategory(categoryId, offset, limit);
 
         return SalesPageDto.createPageDtoList(pageByItemCategory);
     }
 
     // 페이지 단건 조회
     public SalesPageDto findPageByPageId(Long pageId) {
-        SalesPage page = itemPageRepository.findPageByPageId(pageId).orElseThrow(() -> new RuntimeException());
+        SalesPage page = salesPageRepository.findPageByPageId(pageId).orElseThrow(() -> new RuntimeException());
         return new SalesPageDto(page);
     }
 
-    private void itemPageReg(SalesPage page, Map<String, List<Long>> optionAndItem) {
+    private void itemPageReg(SalesPage page, Map<String, Map<Long, Map<String, Boolean>>> optionAndItem) {
+        /*
+        "옵션 이름": {
+            {
+                "아이템id": {
+                    "상품표시이름": 메인상품 표시
+                },
+            },
+        },
+        */
+        optionAndItem.forEach((optionName, itemObject) -> {
+            // itemObject -> <itemId, <itemDisplayName, mainItemFlag>
+            itemObject.forEach((itemId, itemDisplayNameAndMainFlag) -> {
+                ItemSalesPage itemSalesPage = new ItemSalesPage(optionName);
+
+                itemDisplayNameAndMainFlag.forEach((itemDisplayName, mainFlag) -> {
+                    Item item = itemRepository.findOne(itemId).orElseThrow(() -> new RuntimeException("아이템이 존재하지 않습니다"));
+                    itemSalesPage.setItem(item);
+                    itemSalesPage.setSalesPage(page);
+                    itemSalesPage.setIsMainItemAndDisplayName(mainFlag, itemDisplayName);
+                });
+            });
+        });
+
+
+
+        /*
         optionAndItem.forEach((key, value) -> {
+            // key :  옵션 이름. value : List<Map<itemId, itemDisplayName>> 형태로된 데이터
+
             for (Long itemId : value) {
                 log.info("반복");
                 ItemSalesPage itemPage = new ItemSalesPage(key);
@@ -86,14 +115,21 @@ public class SalesPageService {
                 itemPage.setSalesPage(page);
             }
         });
+         */
+
+
     }
 
     // 실제론
     @Transactional
     public void deletePage(Long pageId) {
-        SalesPage page = itemPageRepository.findPageByPageId(pageId).orElseThrow(() -> new RuntimeException());
+        SalesPage page = salesPageRepository.findPageByPageId(pageId).orElseThrow(() -> new RuntimeException());
         page.deletePage(); // 페이지 활성화만 false로 바꾸는걸로 하자
 
+    }
+
+    public List<SalesPage> findSalesPageByItemCategoryAndSearchCond(SearchCond searchCond, Map<String, List<String>> filters, int offset, int limit) {
+        return salesPageRepository.findSalesPageByItemCategoryAndSearchCond(searchCond, filters, offset, limit);
     }
 
 }
