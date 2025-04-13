9e58343f9f54b580acbe1632c230822d8c087403
hyc4841 <dbscjf4841@naver.com>
2025-04-13 22:41:56 +0900

    SalesPageController ItemController에서 아이템 리스트 조회 로직 판매 페이지 리스트 조회 로직으로 이전

--- src/main/java/love/shop/controller/SalesPageController.java
+++ src/main/java/love/shop/controller/SalesPageController.java
@@ -4,18 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import love.shop.domain.category.Category;
 import love.shop.domain.salesPage.SalesPage;
 import love.shop.repository.ItemPage.SalesPageRepository;
 import love.shop.repository.item.ItemRepository;
 import love.shop.service.item.ItemService;
 import love.shop.service.itemPage.SalesPageService;
+import love.shop.web.item.searchCond.SearchCond;
+import love.shop.web.item.searchFilter.SearchFilter;
 import love.shop.web.itemPage.dto.CreatePageReqDto;
 import love.shop.web.itemPage.dto.ModifyPageReqDto;
 import love.shop.web.page.dto.SalesPageDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -28,6 +33,52 @@ public class SalesPageController {
     private final ItemRepository itemRepository;
     private final SalesPageRepository itemPageRepository;
     private final ItemService itemService;
+    private final SalesPageService salesPageService;
+
+    @GetMapping("/items")
+    public ResponseEntity<?> items(@ModelAttribute SearchCond searchCond, // 중분류 카테고리로 뿌려주는걸로 만들자
+                                   @RequestParam Map<String, String> checkedFilter,
+                                   @RequestParam(value = "offset", defaultValue = "0") int offset,
+                                   @RequestParam(value = "limit", defaultValue = "50") int limit) {
+
+        checkedFilter.remove("itemName");
+        checkedFilter.remove("morePrice");
+        checkedFilter.remove("categories");
+        checkedFilter.remove("type");
+        checkedFilter.remove("lessPrice");
+
+        // 1. 스트링으로 들어온 Map<String, String> filter 를 Map<String, List<String>> 으로 변환하기
+        Map<String, List<String>> convertedFilter = new HashMap<>();
+
+        for (String key : checkedFilter.keySet()) {
+            List<String> values = List.of(checkedFilter.get(key).split(","));
+            convertedFilter.put(key, values);
+        }
+
+        log.info("들어온 조건들={}", checkedFilter);
+        log.info("변환한 조건들={}", convertedFilter);
+        log.info("searchCond={}", searchCond);
+
+        // 타입이 없고 카테고리 id만 넘어온다면
+        Category category = itemService.findCategoryById(searchCond.getCategories());
+        String type = category.getType(); // 해당 카테고리의 데이터 타입 확인
+
+        // 클라이언트 쪽에서 보여줄 필터
+        SearchFilter filters = SearchFilter.findFilter(type);
+
+        List<SalesPage> salesPageList = salesPageService.findSalesPageByItemCategoryAndSearchCond(searchCond, convertedFilter, offset, limit);
+        log.info("조건으로 찾은 SalesPage={}", salesPageList);
+        // 검색 조건으로 찾은 아이템 Dto로 변환
+//        List<ItemDto> itemDtoList = ItemDto.createItemDtoList(items);
+
+        // dto 변환
+        List<SalesPageDto> pageDtoList = SalesPageDto.createPageDtoList(salesPageList);
+
+        // 응답 데이터 형태로 감싸기
+        ItemController.ItemListPageResult<Object> objectItemListPageResult = new ItemController.ItemListPageResult<>(pageDtoList, filters, type);
+
+        return ResponseEntity.ok(objectItemListPageResult);
+    }
 
     @PatchMapping("/page/{pageId}")
     public ResponseEntity<?> modifyItemPage(@PathVariable Long pageId, @RequestBody ModifyPageReqDto modifyPageReqDto) {
@@ -65,12 +116,14 @@ public class SalesPageController {
 
         List<SalesPageDto> pageDtoList = itemPageService.findItemPageWithItemCategory(categoryId, offset, limit);
 
+        // 아이템 몇개 채워넣고 카테고리 및 필터링 검색으로 해당 아이템을 가진 판매 페이지 조회로 가보자
+
         return ResponseEntity.ok(pageDtoList);
     }
 
     // 단건 조회인데 왜 페이징이 들어가 있지? 아 리뷰 페이징
     // 판매 페이지 조회
-    @GetMapping("/page/{pageId}")
+    @GetMapping("/salesPage/{pageId}")
     public ResponseEntity<?> findPageByPageId(@PathVariable Long pageId,
                                               @RequestParam(name = "reviewOffset", defaultValue = "0") int reviewOffset,
                                               @RequestParam(name = "reviewLimit", defaultValue = "10") int reviewLimit) {
@@ -84,6 +137,10 @@ public class SalesPageController {
         // 방안 2. 페이지 가져올때
 
         // 페이징 구현하는거 자세히 알기
+
+
+        // 리뷰, 각 판매 페이지에서 페이징으로 가져오는거 구현하기,
+        // 판매 페이지 레이아웃 간단히 만들고, 옵션 구현된거 제대로 꾸며보기
 
         PageResultWrapper<Object> result = new PageResultWrapper<>(pageDto, "ok");
         return ResponseEntity.ok(result);
