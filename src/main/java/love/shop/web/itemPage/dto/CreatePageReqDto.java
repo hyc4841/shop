6f0f2cfb84339cd0754ac92942fd88c59c0e2e6f
hyc4841 <dbscjf4841@naver.com>
2025-04-13 22:37:26 +0900

    CreatePageReqDto 페이지 생성 dto 데이터 구조 변경

--- src/main/java/love/shop/web/itemPage/dto/CreatePageReqDto.java
+++ src/main/java/love/shop/web/itemPage/dto/CreatePageReqDto.java
@@ -7,7 +7,19 @@ import java.util.Map;
 
 @Data
 public class CreatePageReqDto {
-    private Map<String, List<Long>> optionAndItem;
+    private Map<String, Map<Long, Map<String, Boolean>>> optionAndItem;
+    // Map<Long, String> : <itemId, itemDisplayName>
+
+    /*
+        "옵션 이름": {
+            {
+                "아이템id": {
+                    "상품표시이름": 메인상품 표시
+                },
+            },
+        },
+
+     */
 
     private String pageName;
     private List<String> images;
