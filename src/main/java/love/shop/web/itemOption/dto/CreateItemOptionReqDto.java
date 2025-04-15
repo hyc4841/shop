84a20477cd029961891c9d1fd86d0a87d4dbd53a
hyc4841 <dbscjf4841@naver.com>
2025-04-16 01:34:38 +0900

    CreateItemOptionReqDto 계층형 상품 옵션 생성 dto 생성

---
+++ src/main/java/love/shop/web/itemOption/dto/CreateItemOptionReqDto.java
@@ -0,0 +1,16 @@
+package love.shop.web.itemOption.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateItemOptionReqDto {
+
+    private String optionName; // 옵션 이름
+
+    private Long itemId; // 이 옵션에 해당하는 아이템
+
+    private Integer optionNum; // 부모 자식관계 알려고 넣은 옵션 번호
+
+    private Integer parentNum; // 부모 옵션 번호
+
+}
