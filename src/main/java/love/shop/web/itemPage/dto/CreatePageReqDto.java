85afc5b9ff41130751ddee4d96be6ba098950c76
hyc4841 <dbscjf4841@naver.com>
2025-04-16 01:32:30 +0900

    CreatePageReqDto optionAndItem -> optionList 상품 옵션으로 데이터 구조 변경

--- src/main/java/love/shop/web/itemPage/dto/CreatePageReqDto.java
+++ src/main/java/love/shop/web/itemPage/dto/CreatePageReqDto.java
@@ -1,30 +1,22 @@
 package love.shop.web.itemPage.dto;
 
 import lombok.Data;
+import love.shop.web.itemOption.dto.CreateItemOptionReqDto;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class CreatePageReqDto {
-    private Map<String, Map<Long, Map<String, Boolean>>> optionAndItem; // 이렇게 하지 말고 이제 아이템 옵션을 새로 구현해서 그 구조에 맞게 할거임
-    // Map<Long, String> : <itemId, itemDisplayName>
-
-    // optionItem을 계층으로 만들어서 사용할거임.
-    // optionItem 생성에 알맞은 데이터 구조가 들어와야함.
-
-    /*
-        "옵션 이름": {
-            {
-                "아이템id": {
-                    "상품표시이름": 메인상품 표시
-                },
-            },
-        },
-
-     */
-
     private String pageName;
     private List<String> images;
     private String description;
+
+    private List<CreateItemOptionReqDto> optionList;
+
+
+
+
+
+
 }
