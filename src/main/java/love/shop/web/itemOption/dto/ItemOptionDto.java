0ac744b12048a49e24d5cb2f98bb4db0311e98ed
hyc4841 <dbscjf4841@naver.com>
2025-04-16 23:38:34 +0900

    ItemOptionDto dto 데이터 구조 변경

--- src/main/java/love/shop/web/itemOption/dto/ItemOptionDto.java
+++ src/main/java/love/shop/web/itemOption/dto/ItemOptionDto.java
@@ -1,39 +1,80 @@
 package love.shop.web.itemOption.dto;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import love.shop.domain.item.Item;
 import love.shop.domain.itemOption.ItemOption;
 import love.shop.web.item.dto.ItemDto;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+@Slf4j
 @Data
 public class ItemOptionDto {
 
     private Long id;
-    private String optionName;
     private Long parentId;
-//    private List<ItemOptionDto> child;
+    private String optionName;
     private ItemDto item;
     private Long salesPageId;
+    private List<ItemOptionDto> child;
 
-
+    // 자식은 연결 안된 생성자
     public ItemOptionDto(ItemOption itemOption) {
         this.id = itemOption.getId();
         this.optionName = itemOption.getOptionName();
+
         if (itemOption.getParent() != null) {
             this.parentId = itemOption.getParent().getId();
         } else {
             this.parentId = null;
         }
-//        this.child = itemOption.get;
+
         if (itemOption.getItem() != null) {
-            this.item = ItemDto.createItemDto(itemOption.getItem());
+            this.item = ItemDto.createItemDto(Item.proxyToEntity(itemOption.getItem()));
         } else {
             this.item = null;
         }
+
         this.salesPageId = itemOption.getSalesPage().getId();
     }
 
+    public static List<ItemOptionDto> makeItemOptionDto(List<ItemOption> itemOptionList) {
+
+        List<ItemOptionDto> parentList = new ArrayList<>();
+
+        for (ItemOption itemOption : itemOptionList) {
+            if (itemOption.getParent() == null) {
+                ItemOptionDto parentItemOptionDto = makeDepth(itemOptionList, itemOption);
+                parentList.add(parentItemOptionDto);
+            }
+        }
+
+        return parentList;
+    }
+
+    private static ItemOptionDto makeDepth(List<ItemOption> itemOptionList, ItemOption parent) {
+        List<ItemOptionDto> childList = new ArrayList<>();
+
+        ItemOptionDto parentDto = new ItemOptionDto(parent);
+
+        for (ItemOption itemOption : itemOptionList) {
+            if (itemOption.getParent() != null && Objects.equals(itemOption.getParent().getId(), parent.getId())) {
+                log.info("부모 id={}", itemOption.getParent());
+                ItemOptionDto child = makeDepth(itemOptionList, itemOption);
+                childList.add(child);
+            }
+        }
+
+        parentDto.setChild(childList);
+        return parentDto;
+    }
+
+
+    public void setChild(List<ItemOptionDto> child) {
+        this.child = child;
+    }
 
 }
