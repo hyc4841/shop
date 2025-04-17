98795c4097201804e8ee2dde996f4661032e2c81
hyc4841 <dbscjf4841@naver.com>
2025-04-18 00:04:49 +0900

    enum 클래스에서 일반 클래스로 변경

--- src/main/java/love/shop/web/item/filter/tv/TVSpeakerChannel.java
+++ src/main/java/love/shop/web/item/filter/tv/TVSpeakerChannel.java
@@ -1,4 +1,27 @@
 package love.shop.web.item.filter.tv;
 
-public enum TVSpeakerChannel {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TVSpeakerChannel {
+
+    private List<String> tvSpeakerChannel;
+
+    public TVSpeakerChannel() {
+        this.tvSpeakerChannel = createTVSpeakerChannelList();
+    }
+
+    public static List<String> createTVSpeakerChannelList() {
+        List<String> tvSoundList = new ArrayList<>();
+        tvSoundList.add("2.0채널");
+        tvSoundList.add("2.0.2채널");
+        tvSoundList.add("2.1채널");
+        tvSoundList.add("2.1.2채널");
+        tvSoundList.add("3.1.2채널");
+        tvSoundList.add("4.0채널");
+        tvSoundList.add("4.1채널");
+        tvSoundList.add("4.2채널");
+
+        return tvSoundList;
+    }
 }
