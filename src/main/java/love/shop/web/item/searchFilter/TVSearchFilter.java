9511fa659a15888f4cf758704912df54294198b1
hyc4841 <dbscjf4841@naver.com>
2025-04-18 00:05:07 +0900

    TVSearchFilter 필드 선언형 변경

--- src/main/java/love/shop/web/item/searchFilter/TVSearchFilter.java
+++ src/main/java/love/shop/web/item/searchFilter/TVSearchFilter.java
@@ -16,14 +16,14 @@ public class TVSearchFilter extends SearchFilter {
     private List<TVDisplayType> tvDisplayTypes;
     private List<TVHDR> tvHDRs;
     private List<TVManufacturer> tvManufacturers;
-    private List<TVPictureQuality> tvPictureQualities;
+    private List<String> tvPictureQualities;
     private List<TVProcessor> tvProcessors;
     private List<TVRefreshRate> tvRefreshRates;
     private List<TVResolution> tvResolutions;
-    private List<TVScreenSize> tvScreenSizes;
+    private List<String> tvScreenSizes;
     private List<String> tvSounds;
-    private List<TVSpeakerChannel> tvSpeakerChannels;
-    private List<TVSpeakerOutput> tvSpeakerOutputs;
+    private List<String> tvSpeakerChannels;
+    private List<String> tvSpeakerOutputs;
 
     public static TVSearchFilter createTVSearchFilter() {
         TVSearchFilter tvSearchFilter = new TVSearchFilter();
@@ -32,14 +32,14 @@ public class TVSearchFilter extends SearchFilter {
         tvSearchFilter.setTvDisplayTypes(Arrays.asList(TVDisplayType.values()));
         tvSearchFilter.setTvHDRs(Arrays.asList(TVHDR.values()));
         tvSearchFilter.setTvManufacturers(Arrays.asList(TVManufacturer.values()));
-        tvSearchFilter.setTvPictureQualities(Arrays.asList(TVPictureQuality.values()));
+        tvSearchFilter.setTvPictureQualities(TVPictureQuality.createTVPictureQualityList());
         tvSearchFilter.setTvProcessors(Arrays.asList(TVProcessor.values()));
         tvSearchFilter.setTvRefreshRates(Arrays.asList(TVRefreshRate.values()));
         tvSearchFilter.setTvResolutions(Arrays.asList(TVResolution.values()));
-        tvSearchFilter.setTvScreenSizes(Arrays.asList(TVScreenSize.values()));
+        tvSearchFilter.setTvScreenSizes(TVScreenSize.createTVScreenSizeList());
         tvSearchFilter.setTvSounds(TVSound.createTVSoundList()); // 이렇게 그냥 string 으로 해도 괜찮을거 같은데?
-        tvSearchFilter.setTvSpeakerChannels(Arrays.asList(TVSpeakerChannel.values()));
-        tvSearchFilter.setTvSpeakerOutputs(Arrays.asList(TVSpeakerOutput.values()));
+        tvSearchFilter.setTvSpeakerChannels(TVSpeakerChannel.createTVSpeakerChannelList());
+        tvSearchFilter.setTvSpeakerOutputs(TVSpeakerOutput.createTVSpeakerOutputlList());
 
         return tvSearchFilter;
     }
