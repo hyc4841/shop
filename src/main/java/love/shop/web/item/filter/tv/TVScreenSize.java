f996bbb8c25dcf11f2572d6ad6eac513fa1a29c4
hyc4841 <dbscjf4841@naver.com>
2025-04-18 00:04:49 +0900

    enum 클래스에서 일반 클래스로 변경

--- src/main/java/love/shop/web/item/filter/tv/TVScreenSize.java
+++ src/main/java/love/shop/web/item/filter/tv/TVScreenSize.java
@@ -1,6 +1,39 @@
 package love.shop.web.item.filter.tv;
 
-public enum TVScreenSize { // 화면 크기
-    inch_55, inch_65, inch_75, inch_77, inch_50, inch_48, inch_27, inch_32, inch_85, inch_98, inch_100,
-    inch_97, inch_115, inch_86, inch_83, inch_70, inch_42, inch_40, inch_24
+import java.util.ArrayList;
+import java.util.List;
+
+public class TVScreenSize { // 화면 크기
+
+    private List<String> tvScreenSize;
+
+    public TVScreenSize() {
+        this.tvScreenSize = createTVScreenSizeList();
+    }
+
+    public static List<String> createTVScreenSizeList() {
+        List<String> tvPictureQuality = new ArrayList<>();
+        tvPictureQuality.add("24인치");
+        tvPictureQuality.add("27인치");
+        tvPictureQuality.add("32인치");
+        tvPictureQuality.add("40인치");
+        tvPictureQuality.add("42인치");
+        tvPictureQuality.add("48인치");
+        tvPictureQuality.add("50인치");
+        tvPictureQuality.add("55인치");
+        tvPictureQuality.add("65인치");
+        tvPictureQuality.add("70인치");
+        tvPictureQuality.add("75인치");
+        tvPictureQuality.add("77인치");
+        tvPictureQuality.add("83인치");
+        tvPictureQuality.add("85인치");
+        tvPictureQuality.add("86인치");
+        tvPictureQuality.add("97인치");
+        tvPictureQuality.add("98인치");
+        tvPictureQuality.add("100인치");
+        tvPictureQuality.add("115인치");
+
+
+        return tvPictureQuality;
+    }
 }
--- src/main/java/love/shop/web/item/filter/tv/TVSpeakerOutput.java
+++ src/main/java/love/shop/web/item/filter/tv/TVSpeakerOutput.java
@@ -1,4 +1,22 @@
 package love.shop.web.item.filter.tv;
 
-public enum TVSpeakerOutput {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TVSpeakerOutput {
+
+    private List<String> tvSpeakerOutput;
+
+    public TVSpeakerOutput() {
+        this.tvSpeakerOutput = createTVSpeakerOutputlList();
+    }
+
+    public static List<String> createTVSpeakerOutputlList() {
+        List<String> tvSoundList = new ArrayList<>();
+        tvSoundList.add("20W");
+        tvSoundList.add("40W");
+        tvSoundList.add("60W");
+
+        return tvSoundList;
+    }
 }
