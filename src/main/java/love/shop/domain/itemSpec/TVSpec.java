8f488798ab4d6b3074ba7c25c7a68fe76e70a75d
hyc4841 <dbscjf4841@naver.com>
2025-04-19 14:32:27 +0900

    TVSpec 수정

--- src/main/java/love/shop/domain/itemSpec/TVSpec.java
+++ src/main/java/love/shop/domain/itemSpec/TVSpec.java
@@ -1,11 +1,12 @@
 package love.shop.domain.itemSpec;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import love.shop.web.item.filter.tv.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -13,18 +14,36 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class TVSpec {
 
-    private List<TVBrand> tvBrands;
+    @Id
+    @GeneratedValue
+    @Column(name = "tv_spec_id")
+    private Long id;
+
+    @OneToMany(mappedBy = "TvSpec", cascade = CascadeType.PERSIST) // 이거 이름 맞나?
+    private List<TVBrand> tvBrands = new ArrayList<>();
+    @OneToMany
     private List<TVDisplayPanel> tvDisplayPanels;
+    @OneToMany
     private List<TVDisplayType> tvDisplayTypes;
+    @OneToMany
     private List<TVHDR> tvHDRs;
+    @OneToMany
     private List<TVManufacturer> tvManufacturers;
+    @OneToMany
     private List<String> tvPictureQualities;
+    @OneToMany
     private List<TVProcessor> tvProcessors;
+    @OneToMany
     private List<TVRefreshRate> tvRefreshRates;
+    @OneToMany
     private List<TVResolution> tvResolutions;
+    @OneToMany
     private List<String> tvScreenSizes;
+    @OneToMany
     private List<String> tvSounds;
+    @OneToMany
     private List<String> tvSpeakerChannels;
+    @OneToMany
     private List<String> tvSpeakerOutputs;
 
 }
