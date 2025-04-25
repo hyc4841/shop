8d0775e1fd3a0db9506f1a20da3c4043fcf5674c
hyc4841 <dbscjf4841@naver.com>
2025-04-25 23:47:01 +0900

    PageService 임시 상품 판매 페이지 저장문 삭제

--- src/main/java/love/shop/service/page/PageService.java
+++ src/main/java/love/shop/service/page/PageService.java
@@ -19,15 +19,6 @@ public class PageService {
 
     public void savePage(SalesPage page, List<Item> itemList) {
 
-        ItemSalesPage itemPage1 = new ItemSalesPage("이건 책");
-        itemPage1.setSalesPage(page);
-        itemPage1.setItem(itemList.get(0));
-
-        ItemSalesPage itemPage2 = new ItemSalesPage("이건 노트북");
-        itemPage2.setSalesPage(page);
-        itemPage2.setItem(itemList.get(1));
-
-        itemPageRepository.savePage(page);
     }
 
 
