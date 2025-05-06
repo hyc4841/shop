3e0928936bcb422ed2bb484f93eaec96b08253b4
hyc4841 <dbscjf4841@naver.com>
2025-05-06 22:41:39 +0900

    ShopApplication 포트원 관련 Bean 설정

--- src/main/java/love/shop/ShopApplication.java
+++ src/main/java/love/shop/ShopApplication.java
@@ -1,15 +1,20 @@
 package love.shop;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.portone.sdk.server.payment.PaymentClient;
+import io.portone.sdk.server.webhook.WebhookVerifier;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import love.shop.common.configuration.PortOneSecretProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 @EnableCaching // 캐시 활성화
 @SpringBootApplication
+@EnableConfigurationProperties(PortOneSecretProperties.class)
 public class ShopApplication {
 
 	public static void main(String[] args) {
@@ -21,6 +26,16 @@ public class ShopApplication {
 	@Bean
 	JPAQueryFactory jpaQueryFactory() {
 		return new JPAQueryFactory(em);
+	}
+
+	@Bean
+	PaymentClient paymentClient(PortOneSecretProperties secret) {
+		return new PaymentClient(secret.getApi(), "https://api.portone.io", secret.getStoreId());
+	}
+
+	@Bean
+	WebhookVerifier webhookVerifier(PortOneSecretProperties secret) {
+		return new WebhookVerifier(secret.getWebhook());
 	}
 
 	/*
