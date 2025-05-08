bba2280cf0e129f73f17b16500d943f4f681933c
hyc4841 <dbscjf4841@naver.com>
2025-05-08 20:47:59 +0900

    SyncPaymentException 수정

--- src/main/java/love/shop/common/exception/SyncPaymentException.java
+++ src/main/java/love/shop/common/exception/SyncPaymentException.java
@@ -1,4 +1,24 @@
 package love.shop.common.exception;
 
-public class SyncPaymentException extends Exception {
+import lombok.Getter;
+
+@Getter
+public class SyncPaymentException extends RuntimeException {
+
+    private final String errorCode;
+    private final String errorMessage;
+
+    public SyncPaymentException(String errorCode, String errorMessage) {
+        super(errorMessage); // 부모 RuntimeException에 메시지를 전달
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
