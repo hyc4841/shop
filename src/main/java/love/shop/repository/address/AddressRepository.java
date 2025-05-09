f87acc510bfe7646f4fc8991ee3c6fc0f49de207
hyc4841 <dbscjf4841@naver.com>
2025-05-09 19:30:57 +0900

    AddressRepository 멤버 id로 주소 찾기

--- src/main/java/love/shop/repository/address/AddressRepository.java
+++ src/main/java/love/shop/repository/address/AddressRepository.java
@@ -1,10 +1,14 @@
 package love.shop.repository.address;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import love.shop.domain.address.Address;
+import love.shop.domain.address.QAddress;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Slf4j
 @Repository
@@ -13,6 +17,9 @@ import org.springframework.stereotype.Repository;
 public class AddressRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    QAddress address = QAddress.address;
 
     // 주소 저장
     public void save(Address address) {
@@ -22,5 +29,12 @@ public class AddressRepository {
     // 주소 조회
     public Address findAddressById(Long id) {
         return em.find(Address.class, id);
+    }
+
+    // 멤버 id로 주소 조회
+    public List<Address> findAddressesByMemberId(Long memberId) {
+        return queryFactory.selectFrom(address)
+                .where(address.member.id.eq(memberId))
+                .fetch();
     }
 }
