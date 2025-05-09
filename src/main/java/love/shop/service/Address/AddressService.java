10495f9b0bb70f85f0c3da5e71f2954365fe92c0
hyc4841 <dbscjf4841@naver.com>
2025-05-09 19:31:03 +0900

    AddressService 멤버 id로 주소 찾기

--- src/main/java/love/shop/service/Address/AddressService.java
+++ src/main/java/love/shop/service/Address/AddressService.java
@@ -2,8 +2,11 @@ package love.shop.service.Address;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import love.shop.domain.address.Address;
 import love.shop.repository.address.AddressRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -11,5 +14,9 @@ import org.springframework.stereotype.Service;
 public class AddressService {
 
     private final AddressRepository addressRepository;
+
+    public List<Address> findAddressesByMemberId(Long memberId) {
+        return addressRepository.findAddressesByMemberId(memberId);
+    }
 
 }
