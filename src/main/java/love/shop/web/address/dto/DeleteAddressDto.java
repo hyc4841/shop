16fb2c51eaa8e5c21854c0ce03c8604afc2bc5b8
hyc4841 <dbscjf4841@naver.com>
2025-05-16 00:50:31 +0900

    DeleteAddressDto. 검증 어노테이션 추가

--- src/main/java/love/shop/web/address/dto/DeleteAddressDto.java
+++ src/main/java/love/shop/web/address/dto/DeleteAddressDto.java
@@ -1,8 +1,10 @@
 package love.shop.web.address.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class DeleteAddressDto {
+    @NotNull
     private Long addressId;
 }
