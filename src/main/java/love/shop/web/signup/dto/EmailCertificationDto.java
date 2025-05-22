ca07954f6abc014b01d95406f68196884885512c
hyc4841 <dbscjf4841@naver.com>
2025-05-22 23:00:44 +0900

    EmailCertificationDto. 변경 없음

--- src/main/java/love/shop/web/signup/dto/EmailCertificationDto.java
+++ src/main/java/love/shop/web/signup/dto/EmailCertificationDto.java
@@ -3,13 +3,14 @@ package love.shop.web.signup.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import love.shop.domain.member.validator.ValidEmailDuplication;
 
 @Data
 public class EmailCertificationDto {
 
     @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
-//    @ValidEmailDuplication 이거 다시 원상 복구 시켜야함. 지금은 테스트라 주석처리함
+//    @ValidEmailDuplication
     private String email;
 
 }
