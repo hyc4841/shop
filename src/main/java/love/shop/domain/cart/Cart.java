6701e098fe3479fbf0f988533549f666636fd2a5
hyc4841 <dbscjf4841@naver.com>
2025-07-18 16:55:48 +0900

    cart 엔티티. 카트-장바구니 구조로 변경.

--- src/main/java/love/shop/domain/cart/Cart.java
+++ src/main/java/love/shop/domain/cart/Cart.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import love.shop.domain.itemCart.ItemCart;
 import love.shop.domain.member.Member;
+import love.shop.domain.salesPage.SalesPage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,23 +26,14 @@ public class Cart { // 장바구니
     @JoinColumn(name = "member_id")
     private Member member; // 해당 장바구니 주인.
 
+
+
+    /*
+    @OneToMany(mappedBy = "cart")
+    private List<SalesPage> salesPages; // 해당 페이지에서 선택한 아템.
+
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ItemCart> itemCarts = new ArrayList<>(); // 장바구니에 추가한 아이템들
-
-    public void addItemCart(ItemCart itemCart) {
-        // 여기로 들어오는 itemCart는 item과 수량이 설정된 객체임(item 쪽에도 itemCart가 설정된 상태)
-        // cart쪽 itemCart 설정해주기, itemCart에도 cart 썰정해주기
-        this.itemCarts.add(itemCart);
-        itemCart.setCart(this);
-    }
-
-    public Cart(Member member) {
-        this.member = member;
-        member.setCart(this);
-    }
-
-    public void deleteItemCart(ItemCart itemCart) {
-        this.itemCarts.remove(itemCart);
-    }
+     */
 
 }
