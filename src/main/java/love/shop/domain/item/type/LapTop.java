73be3e63bc69c96632a6ae880c70804d32e64d38
hyc4841 <dbscjf4841@naver.com>
2025-07-24 11:07:24 +0900

    LapTop 엔티티. 각 컬럼 enum에서 string 으로 변경.

--- src/main/java/love/shop/domain/item/type/LapTop.java
+++ src/main/java/love/shop/domain/item/type/LapTop.java
@@ -1,14 +1,12 @@
 package love.shop.domain.item.type;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import love.shop.domain.item.*;
-import love.shop.web.item.spec.lapTop.*;
+import love.shop.domain.itemSpec.spec.lapTop.*;
 import love.shop.web.item.saveDto.LapTopSaveReqDto;
 
 @EqualsAndHashCode(callSuper = true)
@@ -17,20 +15,14 @@ import love.shop.web.item.saveDto.LapTopSaveReqDto;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LapTop extends Item {
 
-    @Enumerated(EnumType.STRING)
-    private LapTopBrand lapTopBrand;
-    @Enumerated(EnumType.STRING)
-    private LapTopCpu lapTopCpu;
-    @Enumerated(EnumType.STRING)
-    private LapTopStorage lapTopStorage;
-    @Enumerated(EnumType.STRING)
-    private LapTopScreenSize lapTopScreenSize;
-    @Enumerated(EnumType.STRING)
-    private LapTopManufactureBrand lapTopManufactureBrand;
-
+    private String lapTopBrand;
+    private String lapTopCpu;
+    private String lapTopStorage;
+    private String lapTopScreenSize;
+    private String lapTopManufactureBrand;
     private final String dataType = "LapTop";
 
-    public LapTop(String name, int price, int stockQuantity, LapTopBrand lapTopBrand, LapTopCpu lapTopCpu, LapTopStorage lapTopStorage, LapTopScreenSize lapTopScreenSize, LapTopManufactureBrand lapTopManufactureBrand) {
+    public LapTop(String name, int price, int stockQuantity, String lapTopBrand, String lapTopCpu, String lapTopStorage, String lapTopScreenSize, String lapTopManufactureBrand) {
         super(name, price, stockQuantity);
         this.lapTopBrand = lapTopBrand;
         this.lapTopCpu = lapTopCpu;
@@ -39,14 +31,15 @@ public class LapTop extends Item {
         this.lapTopManufactureBrand = lapTopManufactureBrand;
     }
 
-    public LapTop(LapTopSaveReqDto lapTopDto) {
-        super(lapTopDto.getName(), lapTopDto.getPrice(), lapTopDto.getStockQuantity());
-        this.lapTopBrand = lapTopDto.getLapTopBrand();
-        this.lapTopCpu = lapTopDto.getLapTopCpu();
-        this.lapTopStorage = lapTopDto.getLapTopStorage();
-        this.lapTopScreenSize = lapTopDto.getLapTopScreenSize();
-        this.lapTopManufactureBrand = lapTopDto.getLapTopManufactureBrand();
+    public LapTop(LapTopSaveReqDto laptopDto) {
+        super(laptopDto.getName(), laptopDto.getPrice(), laptopDto.getStockQuantity());
+        this.lapTopBrand = laptopDto.getLapTopBrand();
+        this.lapTopCpu = laptopDto.getLapTopCpu();
+        this.lapTopStorage = laptopDto.getLapTopStorage();
+        this.lapTopScreenSize = laptopDto.getLapTopScreenSize();
+        this.lapTopManufactureBrand = laptopDto.getLapTopManufactureBrand();
     }
+
 
     @Override
     public String getType() {
