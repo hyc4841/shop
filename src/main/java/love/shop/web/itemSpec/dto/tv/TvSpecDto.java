7588ab2d9d149ec1b688b9f96b7adde3587f72e7
hyc4841 <dbscjf4841@naver.com>
2025-07-30 14:04:09 +0900

    TvSpecDto. 필드명 수정

--- src/main/java/love/shop/web/itemSpec/dto/tv/TvSpecDto.java
+++ src/main/java/love/shop/web/itemSpec/dto/tv/TvSpecDto.java
@@ -14,58 +14,58 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class TvSpecDto extends ItemSpecDto {
 
-    private List<TvBrandDto> tvBrandList;
-    private List<TvDisplayPanelDto> tvDisplayPanelList;
-    private List<TvDisplayTypeDto> tvDisplayTypeList;
-    private List<TvHDRDto> tvHDRList;
-    private List<TvManufacturerDto> tvManufacturerList;
-    private List<TvPictureQualityDto> tvPictureQualityList;
-    private List<TvProcessorDto> tvProcessorList;
-    private List<TvRefreshRateDto> tvRefreshRateList;
-    private List<TvResolutionDto> tvResolutionList;
-    private List<TvScreenSizeDto> tvScreenSizeList;
-    private List<TvSoundDto> tvSoundList;
-    private List<TvSpeakerChannelDto> tvSpeakerChannelList;
-    private List<TvSpeakerOutputDto> tvSpeakerOutputList;
+    private List<TvBrandDto> tvBrands;
+    private List<TvDisplayPanelDto> tvDisplayPanels;
+    private List<TvDisplayTypeDto> tvDisplayTypes;
+    private List<TvHDRDto> tvHDRs;
+    private List<TvManufacturerDto> tvManufacturers;
+    private List<TvPictureQualityDto> tvPictureQualities;
+    private List<TvProcessorDto> tvProcessors;
+    private List<TvRefreshRateDto> tvRefreshRates;
+    private List<TvResolutionDto> tvResolutions;
+    private List<TvScreenSizeDto> tvScreenSizes;
+    private List<TvSoundDto> tvSounds;
+    private List<TvSpeakerChannelDto> tvSpeakerChannels;
+    private List<TvSpeakerOutputDto> tvSpeakerOutputs;
 
     public TvSpecDto(TvSpec tvSpec) {
-        this.tvBrandList = tvSpec.getTvBrand().stream()
+        this.tvBrands = tvSpec.getTvBrand().stream()
                 .map(spec -> new TvBrandDto(spec))
                 .collect(Collectors.toList());
-        this.tvDisplayPanelList = tvSpec.getTvDisplayPanels().stream()
+        this.tvDisplayPanels = tvSpec.getTvDisplayPanels().stream()
                 .map(spec -> new TvDisplayPanelDto(spec))
                 .collect(Collectors.toList());
-        this.tvDisplayTypeList = tvSpec.getTvDisplayTypes().stream()
+        this.tvDisplayTypes = tvSpec.getTvDisplayTypes().stream()
                 .map(spec -> new TvDisplayTypeDto(spec))
                 .collect(Collectors.toList());
-        this.tvHDRList = tvSpec.getTvHDRS().stream()
+        this.tvHDRs = tvSpec.getTvHDRS().stream()
                 .map(spec -> new TvHDRDto(spec))
                 .collect(Collectors.toList());
-        this.tvManufacturerList = tvSpec.getTvManufacturers().stream()
+        this.tvManufacturers = tvSpec.getTvManufacturers().stream()
                 .map(spec -> new TvManufacturerDto(spec))
                 .collect(Collectors.toList());
-        this.tvPictureQualityList = tvSpec.getTvPictureQualities().stream()
+        this.tvPictureQualities = tvSpec.getTvPictureQualities().stream()
                 .map(spec -> new TvPictureQualityDto(spec))
                 .collect(Collectors.toList());
-        this.tvProcessorList = tvSpec.getTvProcessors().stream()
+        this.tvProcessors = tvSpec.getTvProcessors().stream()
                 .map(spec -> new TvProcessorDto(spec))
                 .collect(Collectors.toList());
-        this.tvRefreshRateList = tvSpec.getTvRefreshRates().stream()
+        this.tvRefreshRates = tvSpec.getTvRefreshRates().stream()
                 .map(spec -> new TvRefreshRateDto(spec))
                 .collect(Collectors.toList());
-        this.tvResolutionList = tvSpec.getTvResolutions().stream()
+        this.tvResolutions = tvSpec.getTvResolutions().stream()
                 .map(spec -> new TvResolutionDto(spec))
                 .collect(Collectors.toList());
-        this.tvScreenSizeList = tvSpec.getTvScreenSizes().stream()
+        this.tvScreenSizes = tvSpec.getTvScreenSizes().stream()
                 .map(spec -> new TvScreenSizeDto(spec))
                 .collect(Collectors.toList());
-        this.tvSoundList = tvSpec.getTvSounds().stream()
+        this.tvSounds = tvSpec.getTvSounds().stream()
                 .map(spec -> new TvSoundDto(spec))
                 .collect(Collectors.toList());
-        this.tvSpeakerChannelList = tvSpec.getTvSpeakerChannels().stream()
+        this.tvSpeakerChannels = tvSpec.getTvSpeakerChannels().stream()
                 .map(spec -> new TvSpeakerChannelDto(spec))
                 .collect(Collectors.toList());
-        this.tvSpeakerOutputList = tvSpec.getTvSpeakerOutputs().stream()
+        this.tvSpeakerOutputs = tvSpec.getTvSpeakerOutputs().stream()
                 .map(spec -> new TvSpeakerOutputDto(spec))
                 .collect(Collectors.toList());
     }
